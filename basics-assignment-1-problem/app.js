42e9c0bb99a94f764fd965016f641a391daa99b8
lucaString <136691887+lucaString@users.noreply.github.com>
2023-06-27 09:28:02 +0200

    Add files via upload

---
+++ basics-assignment-1-problem/app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            name: 'Luca',
+            age: 33,
+            imgUrl: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSGjgav8XmbNHyyG5bgTQwz_1EWWWtKu3Tj_mRC6ggEa9vfk8DW'
+        };
+    }
+});
+
+app.mount('#assignment');
+
